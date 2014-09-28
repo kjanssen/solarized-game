3f52928e57b81b590487d65f7ddcee07bd0d6e3b
Kyle Janssen <kyle.janssen.0@gmail.com>
2014-09-28 11:25:41 -0700

    First commit. Created libGDX project.

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an as-of-yet-unnamed project that seeks to create a 2D platformer
+utilizing the Solarized color scheme by Ethan Schoonover.
---
+++ desktop/src/com/kylejanssen/game/solarized/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kylejanssen.game.solarized.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kylejanssen.game.solarized.SolarizedGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SolarizedGame(), config);
+	}
+}
