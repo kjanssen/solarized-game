b9877b35fea974fffc63b590280dd89abd8b29cd
Kyle Janssen <kyle.janssen.0@gmail.com>
2014-09-29 11:53:31 -0700

    Added a player square that moves around.

--- core/src/com/kylejanssen/game/solarized/GameScreen.java
+++ core/src/com/kylejanssen/game/solarized/GameScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -12,18 +13,22 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 public class GameScreen implements Screen {
 
     private SolarizedGame game;
+    private Player player;
     private OrthographicCamera camera;
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
+    private ShapeRenderer shapeRenderer;
 
-    private static final float HEIGHT_VIEW = 16f;
-    private static final float WIDTH_VIEW = 28.4444f;
+    private static final float HEIGHT_VIEW = 18f;
+    private static final float WIDTH_VIEW = 32f;
     private static final float SIZE_TILE = 32f;
     private float WIDTH_MAP;
 
     public GameScreen(SolarizedGame game) {
         this.game = game;
 
+        player = new Player(4f, 3f);
+        shapeRenderer = new ShapeRenderer();
         map = new TmxMapLoader().load("maps/solarized-1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1 / SIZE_TILE);
 
@@ -39,13 +44,27 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            camera.position.x -= delta * 10;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.J)) {
+            //camera.position.x += delta * 10;
+            player.y -= delta * 10;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            camera.position.x += delta * 10;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.K)) {
+            //camera.position.x -= delta * 10;
+            player.y += delta * 10;
         }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.H)) {
+            //camera.position.x -= delta * 10;
+            player.x -= delta * 10;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.L)) {
+            //camera.position.x += delta * 10;
+            player.x += delta * 10;
+        }
+
+        camera.position.x = player.x;
 
         float offset = WIDTH_VIEW / 2;
 
@@ -60,6 +79,12 @@ public class GameScreen implements Screen {
         camera.update();
         renderer.setView(camera);
         renderer.render();
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(108 / 255f, 113 / 255f, 196 / 255f, 1);
+        //shapeRenderer.circle(player.x, player.y, player.radius);
+        shapeRenderer.rect(player.x, player.y, 1f, 1f);
+        shapeRenderer.end();
     }
 
     @Override
---
+++ core/src/com/kylejanssen/game/solarized/Player.java
@@ -0,0 +1,10 @@
+package com.kylejanssen.game.solarized;
+
+import com.badlogic.gdx.math.Circle;
+
+public class Player extends Circle {
+
+    public Player(float x, float y) {
+        super(x, y, 0.5f);
+    }
+}
